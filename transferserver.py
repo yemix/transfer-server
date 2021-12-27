d39398b36cf1a584c7003e3fd0c3a389a5af3aa9
yemix <37916923+yemix@users.noreply.github.com>
2021-12-27 03:46:34 +0100

    Update transferserver.py

--- transferserver.py
+++ transferserver.py
@@ -15,7 +15,7 @@ proxyurl = data["customPROXY-URL"]
 
 headers = {
 	"authorization": token,
-    "content-type": "application/json",
+	"content-type": "application/json",
 	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.1011 Chrome/91.0.4472.164 Electron/13.4.0 Safari/537.36"
 }
 
